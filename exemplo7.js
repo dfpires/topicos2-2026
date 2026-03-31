7c51878e9ff6306bfbf9ecff79e9a219ec005c26
Daniel <dfpires@gmail.com>
2026-03-31 09:11:21 -0300

    exe7

--- exemplo7.js
+++ exemplo7.js
@@ -1,32 +1,68 @@
 import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native'
 import {MaterialCommunityIcons} from '@expo/vector-icons'
 import {useState} from 'react'
-export default function App(){
-    const [nome, setNome] = useState("")
-    const [email, setEmail] = useState("")
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+
+const Stack = createStackNavigator()
+
+function FormScreen({navigation}){
+        const [nome, setNome] = useState("")
+        const [email, setEmail] = useState("")
+        const enviar = () => {
+            const no = nome.trim();
+            const em = email.trim();
+            if (!no || !em) return; // sai da função
+            // navegação para outra página
+            navigation.navigate("Detalhes", {nome: no, email: em})
+        }
+        return (
+            <View style={styles.container}>
+                <MaterialCommunityIcons 
+                    name="account-edit"
+                    size={80}
+                    color="#6366F1"
+                    style={styles.iconHeader}
+                    />
+                <Text style={styles.titulo}> Cadastro Rápido </Text>
+                <View style={styles.card}>
+                    <Text style={styles.label}> Nome completo </Text>
+                    <TextInput style={styles.input} value={nome} onChangeText={setNome}/>
+                    <Text style={styles.label}> E-mail </Text>
+                    <TextInput style={styles.input} value={email} onChangeText={setEmail}/>
+                    <TouchableOpacity style={styles.botao} onPress={enviar}>
+                        <Text style={styles.textoBotao}> Avançar </Text>
+                        <MaterialCommunityIcons name="arrow-right" size={20}
+                            color="#FFF"/>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        )
+}
+function DetailScreen({route, navigation}){
+    // recupera os dados passados pra ele
+    const {nome, email} = route.params ?? {nome: "N/A", email: "N/A"}
     return (
-        <View style={styles.container}>
-            <MaterialCommunityIcons 
-                name="account-edit"
-                size={80}
-                color="#6366F1"
-                style={styles.iconHeader}
-                />
-            <Text style={styles.titulo}> Cadastro Rápido </Text>
-            <View style={styles.card}>
-                <Text style={styles.label}> Nome completo </Text>
-                <TextInput style={styles.input} value={nome} onChangeText={setNome}/>
-                <Text style={styles.label}> E-mail </Text>
-                <TextInput style={styles.input} value={email} onChangeText={setEmail}/>
-                <TouchableOpacity style={styles.botao}>
-                    <Text style={styles.textoBotao}> Avançar </Text>
-                    <MaterialCommunityIcons name="arrow-right" size={20}
-                        color="#FFF"/>
-                </TouchableOpacity>
+        <View>
+            <Text> Confirme seus dados </Text>
+            <View>
+                <Text> Nome: </Text> <Text> {nome} </Text>
+                <Text> E-Mail: </Text> <Text> {email} </Text>
             </View>
         </View>
     )
 }
+export default function App(){
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Form">
+                <Stack.Screen name="Form" component={FormScreen} options={{title:"Início"}}/>
+                <Stack.Screen name="Detalhes" component={DetailScreen} options={{title:"Resumo"}}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+        
 const styles = StyleSheet.create({
     container: { flex: 1, backgroundColor: "#F1F5F9", padding: 20 },
     iconHeader: { alignSelf: 'center', marginTop: 20 },
